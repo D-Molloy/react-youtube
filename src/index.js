60594a6ccd8c5a8a84c1acf88ed67fb2710104f5
Denis Molloy <denis.molloy@gmail.com>
2018-04-21 16:32:32 -0700

    added YTSearch to index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import YTSearch from "youtube-api-search";
 
 import SearchBar from "./components/search_bar.js";
 
-// const API_KEY = "AIzaSyAQYiNuD6trK-giyef9h6J44-uUymn-6T0";
+const API_KEY = "AIzaSyAQYiNuD6trK-giyef9h6J44-uUymn-6T0";
+
 
 // Component - something that produces HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      videos: [],
+    }
+
+    YTSearch({key: API_KEY, term: "phish"}, (videos) =>{
+      //destructuring (ES6) the response and saving it to videos
+      this.setState({videos})
+      //same as: ---key and value must have the same name
+      // this.setState({videos: videos})
+
+    })
+        
+    YTSearch({key: API_KEY, term: "phish"}, (data) =>{
+      this.setState({videos: data})
+    })
+
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
 
 //  Take this component's generated HTML and put it in the DOM
 ReactDOM.render(<App />, document.querySelector(".container"));
